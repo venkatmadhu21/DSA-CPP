023e48c579eafb0c8025c1049b8de11c7d986843
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-05-14 21:26:35 +0530

    Time: 3 ms (62.32%), Space: 24.4 MB (8.96%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,35 @@
+ 
+class MinStack {
+private:
+    stack<int> mainStack;
+    stack<int> minStack;
+
+public:
+    MinStack() {
+        // Constructor initializes empty stacks
+    }
+
+    void push(int val) {
+        mainStack.push(val);
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.top());
+        }
+    }
+
+    void pop() {
+        if (!mainStack.empty()) {
+            mainStack.pop();
+            minStack.pop();
+        }
+    }
+
+    int top() {
+        return mainStack.top();
+    }
+
+    int getMin() {
+        return minStack.top();
+    }
+};
