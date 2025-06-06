6896a28266fedf090e7121d0af03a8771d0f08ab
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-06-06 15:24:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-         double ans=1.0;
-         long long nn=n;
-         if(nn<0) nn=-1* nn;
-         while(nn){
-            if(nn%2){
-                ans=ans*x;
-                nn=nn-1;
-            }else{
-                x*=x;
-                nn=nn/2;
+        long long nn = n;  // Use long long to avoid overflow
+        double ans = 1.0;
+
+        if (nn < 0) {
+            nn = -1 * nn;
+        }
+
+        while (nn) {
+            if (nn % 2 == 1) {
+                ans *= x;
+                nn--;
+            } else {
+                x *= x;
+                nn /= 2;
             }
-         }
-         if(n<0) ans=(double)(1.0) / (double) (ans);
-         return ans;
-         
+        }
+
+        if (n < 0) ans = 1.0 / ans;
+        return ans;
     }
 };
