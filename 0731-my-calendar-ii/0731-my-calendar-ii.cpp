73511ad7fc0a30ec33640631584235fb2d6bd91a
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-08-10 15:11:53 +0530

    Time: 25 ms (68.9%), Space: 39.4 MB (93.22%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -2,19 +2,15 @@ class MyCalendarTwo {
 private:
     vector<pair<int, int>> booked;
     vector<pair<int, int>> overlaps;
-
 public:
     MyCalendarTwo() {}
-
     bool book(int start, int end) {
-        // Step 1: Check triple booking
+
         for (auto &b : overlaps) {
             if (start < b.second && b.first < end) {
-                return false; // Triple booking detected
+                return false; 
             }
         }
-
-        // Step 2: Record new double bookings
         for (auto &b : booked) {
             if (start < b.second && b.first < end) {
                 int overlapStart = max(start, b.first);
@@ -22,8 +18,6 @@ public:
                 overlaps.push_back({overlapStart, overlapEnd});
             }
         }
-
-        // Step 3: Add to booked
         booked.push_back({start, end});
         return true;
     }
