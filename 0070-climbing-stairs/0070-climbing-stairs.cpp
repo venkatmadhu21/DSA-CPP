8878871448e0fa290497e0c8411d21730b4fec6f
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-09-30 18:48:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,11 @@
 class Solution {
     public: 
     int climbStairs(int n) {
-    if (n <= 2) return n;   // base cases
-
+    if (n <= 2) return n;  
     int a = 1, b = 2, ways;  
     for (int i = 3; i <= n; i++) {
-        ways = a + b;  // f(n) = f(n-1) + f(n-2)
-        a = b;         // shift window
+        ways = a + b;  
+        a = b;
         b = ways;
     }
     return b;
