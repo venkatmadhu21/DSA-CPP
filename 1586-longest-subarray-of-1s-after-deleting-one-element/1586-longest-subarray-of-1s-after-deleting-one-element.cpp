72c1ec531cddfbd4efc81abb14617c48113f65b5
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-10-02 11:41:53 +0530

    Time: 0 ms (100%), Space: 60.3 MB (13.73%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int ans = 0;   // stores the maximum length found
-        int curr = 0;  // current streak of 1's
-        int prev = 0;  // previous streak of 1's before a 0
-        
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                curr++;  // extend current streak of 1’s
-            } else {
-                ans = max(ans, prev + curr);  // "delete" this 0 and join prev+curr
-                prev = curr;                  // move curr to prev (since 0 breaks the chain)
-                curr = 0;                     // reset current streak
+        int ans=0,curr=0,prev=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1) curr++;
+            else{
+                ans=max(ans,prev+curr);
+                prev=curr;
+                curr=0;
             }
         }
-        
-        // Special case: if the entire array is all 1's
-        if (curr == nums.size()) {
-            return curr - 1;  // we must delete 1 element
-        }
-        
-        return max(ans, prev + curr); // final merge in case array ends with 1’s
+        if(curr==nums.size())return curr -1;
+        return max(ans,prev+curr);
     }
 };
