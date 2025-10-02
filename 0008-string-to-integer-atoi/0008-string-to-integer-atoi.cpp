4dc4ac7dbe2f14f1ad54a9713f33633e9fd3fb00
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-10-02 09:35:25 +0530

    Time: 0 ms (100%), Space: 9 MB (97.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -10,8 +10,8 @@ public:
             i++;
         }
         while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            if (result > (INT_MAX - digit) / 10) {
+            int digit = s[i] - '0'; //characters are stored as ASCII so '0'.0
+            if (result > (INT_MAX - digit) / 10) { //checking if the res exceeds if it is multiplied by 10 in the next step.
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
             result = result * 10 + digit;
