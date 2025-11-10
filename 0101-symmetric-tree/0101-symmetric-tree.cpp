9429cb70cb786912e660f1f2e2527eafcfe1152f
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-11-10 21:43:01 +0530

    Time: 0 ms (100%), Space: 18.3 MB (85.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -15,7 +15,8 @@ public:
     bool isMirror(TreeNode* t1, TreeNode* t2){
         if(t1==NULL && t2==NULL)return true;
         if(t1==NULL || t2==NULL)return false;
-        return (t1->val == t2->val) && isMirror(t1->left,t2->right) && isMirror(t1->right,t2->left);
+        return (t1->val == t2->val) && isMirror(t1->left,t2->right) && 
+        isMirror(t1->right,t2->left);
     }
     bool isSymmetric(TreeNode* root) {
         if(root == NULL)return true;
