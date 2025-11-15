5803b882ba3787e04d021ba48c63d2a8ba0e3f34
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2025-11-15 19:54:54 +0530

    Time: 20 ms (78.19%), Space: 93.4 MB (16.51%) - LeetHub

---
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n+1, vector<int>(n+1, 0));
+        
+        // Apply diff array updates
+        for (auto &q : queries) {
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+
+            diff[r1][c1] += 1;
+            if (c2 + 1 < n) diff[r1][c2 + 1] -= 1;
+            if (r2 + 1 < n) diff[r2 + 1][c1] -= 1;
+            if (r2 + 1 < n && c2 + 1 < n) diff[r2 + 1][c2 + 1] += 1;
+        }
+
+        // Prefix sum horizontally
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                diff[i][j] += diff[i][j - 1];
+            }
+        }
+
+        // Prefix sum vertically
+        for (int j = 0; j < n; j++) {
+            for (int i = 1; i < n; i++) {
+                diff[i][j] += diff[i - 1][j];
+            }
+        }
+
+        // Trim matrix to n x n
+        vector<vector<int>> result(n, vector<int>(n));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                result[i][j] = diff[i][j];
+
+        return result;
+    }
+};
