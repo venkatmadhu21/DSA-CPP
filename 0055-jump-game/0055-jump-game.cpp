2af76e0a67834be49a5b9d41cacc3b3b987193df
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2026-02-02 05:40:01 +0530

    Time: 0 ms (100%), Space: 52.3 MB (42.06%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,11 +3,10 @@ public:
     bool canJump(vector<int>& nums) {
         long long far=0;
         for(int i=0;i<nums.size();i++){
-            if( i > far )return false;
-            far= max(far,(long long)i + nums[i]);
+            if(i  > far ) return false;
+            far = max(far,(long long)i+nums[i]);
             if(far >= nums.size()-1) return true;
         }
-        return true ;
+        return true;
     }
-
 };
