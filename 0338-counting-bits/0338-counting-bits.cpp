21dd28f439899122f8d6360de511534cf3c3e06f
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2026-02-03 20:22:06 +0530

    Time: 0 ms (100%), Space: 11.2 MB (33.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int Count(int num ){
-        int count=0;
-        while(num>0){
-            num=num & (num -1);
-            count ++;
-        }
-        return count;
-    }
+int solve(int i){
+    int ans = __builtin_popcount(i);
+    return ans;
+}
     vector<int> countBits(int n) {
-        vector<int> res(n+1);
-            for(int i=0;i<=n;i++){
-                res[i]=Count(i);
-            }
-            return res;
+        vector<int>res;
+        for(int i=0;i<=n;i++){
+            res.push_back(solve(i));
         }
-
+        return res;
+    }
 };
