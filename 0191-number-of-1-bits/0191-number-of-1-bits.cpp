4e4a84b243fb54def1f16035206ffccd5610380f
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2026-02-03 19:33:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.26%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    int hammingWeight(int n) {
-        int cnt=0;
-        while(n){
-            n=n&(n-1);
-            cnt++;
-        }
-        return cnt;
-    }
+    // int hammingWeight(int n) {
+    //     int cnt=0;
+    //     while(n){
+    //         n=n&(n-1);
+    //         cnt++;
+    //     }
+    //     return cnt;
+    // }
+     int hammingWeight(int n) {
+        int count = __builtin_popcount(n);
+        return count;
+     }
 };
