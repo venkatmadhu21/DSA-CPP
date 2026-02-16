b21ca5fc8cdb8b7700c661cd291684e069ffb223
Venkat Madhu Mohan <155867309+venkatmadhu21@users.noreply.github.com>
2026-02-16 08:31:02 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.71%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xoor =0;
-        for(int i=0;i<nums.size();i++){
-            xoor = xoor ^ nums[i];
+        int xorr= 0;
+        for(int i : nums){
+            xorr=xorr ^ i;
         }
-        return xoor;
+        return xorr;
     }
 };
